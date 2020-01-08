be622857b5970680914c8d46f96586b100e6eaab
Jordan Bertieaux <jordan.bertieaux@std.heh.be>
2020-01-08 01:46:07 +0100

    Debug Metasploit MSRPC LOGIN FAILED

--- Metasploit/Metasploit.py
+++ Metasploit/Metasploit.py
@@ -3,8 +3,8 @@ from pymetasploit3.msfrpc import MsfRpcClient
 
 class Metasploit:
     def __init__(self, password, user):
-        self._client = MsfRpcClient(password, port=55553)
-        self._client.login(user, password)
+        self._client = MsfRpcClient(password=password, port=55553)
+        self._client.login(user=user, password=password)
         res = self._client.call('console.create')
         self._console_id = res['id']
 
