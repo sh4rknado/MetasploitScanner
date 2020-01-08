4957c7486710aa44ca04424e84e295c079c0d75b
Jordan Bertieaux <jordan.bertieaux@std.heh.be>
2020-01-08 02:24:37 +0100

    Add Msg auth success

--- Metasploit/Metasploit.py
+++ Metasploit/Metasploit.py
@@ -5,6 +5,10 @@ class Metasploit:
     def __init__(self, password, user):
         self._client = MsfRpcClient(password=password, port=55556)
         self._client.login(user=user, password=password)
+        if self._client.authenticated:
+            print("[SUCESS] Authentification MSGRPC SUCESS")
+        else:
+            print("[ERROR] Authentification ERROR !")
         res = self._client.call('console.create')
         self._console_id = res['id']
 
