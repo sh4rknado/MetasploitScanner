54b42100288422b62c1499fbd356de8662f20de9
Jordan Bertieaux <jordan.bertieaux@std.heh.be>
2020-01-08 21:58:57 +0100

    client waiting

--- Metasploit/Metasploit.py
+++ Metasploit/Metasploit.py
@@ -15,12 +15,12 @@ class Metasploit:
         else:
             print("[ERROR] Authentification ERROR !")
 
-        self._console = MsfRpcConsole(self._client, cb=self.read_console)
         self.client_Isbusy = False
-        self._console_read = list()
-        self._console_data = None
 
     def read_console(self, console_data):
+        console_read = list()
+        console_data = None
+
         self.client_Isbusy = console_data['busy']
         # print("Console State : " + str(self._console_busy))
 
@@ -29,17 +29,15 @@ class Metasploit:
 
             for line in sigdata:
                 if '[+]' in line:
-                    self._console_read.append(line)
-        # self._console_data = console_data
-        # print("\n\n")
-        # print(self._console_data)
-        # print("\n\n")
-        # print("\n\n")
+                    console_read.append(line)
+
         print(console_data['data'])
 
     def send_cmd(self, cmd):
+        console = MsfRpcConsole(self._client, cb=self.read_console)
+
         if self._client.authenticated and not self.client_Isbusy:
-            self._console.execute(cmd)
+            console.execute(cmd)
             time.sleep(5)
         elif self.client_Isbusy:
             print("[WAITING] Client was busy !")
@@ -47,7 +45,7 @@ class Metasploit:
                 time.sleep(5)
 
             print("[INFOS] Client Available now !")
-            self._console.execute(cmd)
+            self.send_cmd(cmd)
             time.sleep(5)
         else:
             print("[ERROR] Client Was Not Authentificated !")
